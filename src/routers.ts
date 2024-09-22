b3c9260d44d274888d662882a2b3daed5676f7bd
wassim <wassimrached404@gmail.com>
2024-09-22 08:56:13 +0100

    added health endpoint

--- src/routers.ts
+++ src/routers.ts
@@ -18,6 +18,10 @@ export function handleRoutes(app: Express) {
     res.send("Central Authentication Service is up and running");
   });
 
+  app.get('/api/health', (req: Request, res: Response) => {
+    res.json({ status: 'UP' });
+  })
+
   app.post("/api/token", async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
